6162d3338fbdc812dee58d242f59b6fe329b46bd
NaufalPujiMahdy <naufalpm230800@gmail.com>
2023-06-12 13:58:50 +0700

    Nambah tabel berita

---
+++ database/migrations/2023_06_12_023404_create_beritas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeritasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beritas', function (Blueprint $table) {
+            $table->increments('id_berita');
+            $table->string('judul');
+            $table->text('isi');
+            $table->date('tgl');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beritas');
+    }
+}
